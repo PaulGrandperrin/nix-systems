ea36a89bf18609bcd964706927909cb1c6644fcf
Paul Grandperrin <paul.grandperrin@gmail.com>
2021-12-10 13:35:51 +0100

    users/paulg/home-desktop.nix

--- users/paulg/home-desktop.nix
+++ users/paulg/home-desktop.nix
@@ -1,20 +1,47 @@
 {pkgs, ...}: {
+  targets.genericLinux.enable = true;
   home = {
     packages = with pkgs; [
       gnome.gnome-tweaks
       glxinfo
       vulkan-tools
       vlc
+      waydroid
+      kate
     ];
     sessionVariables = {
     };
 
   };
 
+  # nixgl or hardware.opengl.setLdLibraryPath = true;
+
+
   systemd.user.sessionVariables = {
   };
 
+  qt = {
+    enable = true;
+    platformTheme = "gnome";
+    style = {
+      package = pkgs.adwaita-qt;
+      name = "adwaita";
+    };
+  };
+
+  services = {
+    #flameshot.enable = true; #TODO
+  };
+
   programs = {
+    foot.enable = true;
+    kitty.enable = true; 
+    alacritty.enable = true;
+    mangohud.enable = true;
+    vscode = {
+      enable = true;
+      extensions = [];
+    };
     terminator = {
       enable = true;
       config = {};
@@ -41,6 +68,12 @@
           "browser.search.region" = "US";
           #"identity.fxaccounts.account.device.name" = "${networking.hostName}";
           "fission.autostart" = true;
+          "apz.overscroll.enabled" = true;
+          "gfx.webrender.compositor.force-enabled" = true;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+          "browser.newtabpage.activity-stream.showSponsored" = false;
+          "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+          "network.IDN_show_punycode" = true;
         };
       };
 
