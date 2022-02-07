8e750f0a48cb30ec3034b91827819cd76485c5fa
Paul Grandperrin <paul.grandperrin@gmail.com>
2022-02-08 00:51:12 +0100

    nixos: opensmtpd

--- nixos/common.nix
+++ nixos/common.nix
@@ -56,6 +56,20 @@
 
   #boot.kernelPackages = pkgs.linuxPackages_latest; # breakes ZFS sometimes
   boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+
+  services.opensmtpd = {
+    enable = true;
+    setSendmail = true;
+    serverConfiguration = ''
+      #listen on localhost
+      table secrets file://etc/nixos/secrets/smtpd
+      table aliases { "@" = "paul.grandperrin+${config.networking.hostName}@gmail.com" } # catch all
+      action "forward" forward-only virtual <aliases> # forward all local mails
+      action "relay" relay host smtp+tls://gmailcreds@smtp.gmail.com:587 auth <secrets> # mail-from "paul.grandperrin@gmail.com"
+      match for local action "forward"
+      match for any action "relay"
+    '';
+  };
   
   ## way too long to build
   #boot.kernelPatches = [{
