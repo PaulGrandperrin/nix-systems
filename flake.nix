6e59e5bbdec156fa0f465378c81b77d61225d5b7
Paul Grandperrin <paul.grandperrin@gmail.com>
2022-02-08 23:13:11 +0100

    macbook: better suspend logic

--- flake.nix
+++ flake.nix
@@ -267,15 +267,24 @@
             services.mbpfan.enable = true;
 
             powerManagement = {
-              # brcmfmac being loaded during hibernation would not let a successful resume
-              # https://bugzilla.kernel.org/show_bug.cgi?id=101681#c116.
-              # Also brcmfmac could randomly crash on resume from sleep.
-              # And also, brcmfac prevents suspending
               powerDownCommands = lib.mkBefore ''
+                # brcmfmac being loaded during hibernation would not let a successful resume
+                # https://bugzilla.kernel.org/show_bug.cgi?id=101681#c116.
+                # Also brcmfmac could randomly crash on resume from sleep.
+                # And also, brcmfac prevents suspending
                 ${pkgs.kmod}/bin/rmmod brcmfmac
                 #echo disabled > /sys/bus/pci/devices/0000:03:00.0/power/wakeup # ARPT in /proc/acpi/wakeup, wifi adapter always wakes up the machine, already disabled by rmmod
-                echo disabled > /sys/bus/acpi/devices/PNP0C0D:00/power/wakeup # LID0 in /proc/acpi/wakeup, wakes up the machine when the lid is in open position
-                echo disabled > /sys/bus/pci/devices/0000:00:14.0/power/wakeup # XHC1 in /proc/acpi/wakeup, USB controller, sometimes wakes up the machine
+
+                # if the LID is open
+                if grep open /proc/acpi/button/lid/LID0/state; then
+                  # disable the open-lid sensor but enable the keyboard (USB) wake up events
+                  echo enabled > /sys/bus/pci/devices/0000:00:14.0/power/wakeup # XHC1 in /proc/acpi/wakeup, USB controller, sometimes wakes up the machine
+                  echo disabled > /sys/bus/acpi/devices/PNP0C0D:00/power/wakeup # LID0 in /proc/acpi/wakeup, wakes up the machine when the lid is in open position
+                else 
+                  # enable the open-lid sensor wake events but disable to USB controller to be extra sure
+                  echo enabled > /sys/bus/acpi/devices/PNP0C0D:00/power/wakeup # LID0 in /proc/acpi/wakeup, wakes up the machine when the lid is in open position
+                  echo disabled > /sys/bus/pci/devices/0000:00:14.0/power/wakeup # XHC1 in /proc/acpi/wakeup, USB controller, sometimes wakes up the machine
+                fi
               '';
               powerUpCommands = lib.mkBefore "${pkgs.kmod}/bin/modprobe brcmfmac";
             };
