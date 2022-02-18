cb107addedb159289edf3a6a438002fa815c6ff4
Paul Grandperrin <paul.grandperrin@gmail.com>
2022-02-18 23:41:58 +0100

    macbookpaul: rust stable

--- flake.nix
+++ flake.nix
@@ -326,7 +326,7 @@
             home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = {inherit system inputs; installDesktopApp = true;};
             home-manager.users.root  = { imports = [./home-manager/cmdline.nix ./home-manager/cmdline-root.nix];};
-            home-manager.users.paulg = { imports = [./home-manager/cmdline.nix ./home-manager/cmdline-user.nix ./home-manager/desktop.nix ./home-manager/desktop-linux.nix ./home-manager/rust-nightly.nix];};
+            home-manager.users.paulg = { imports = [./home-manager/cmdline.nix ./home-manager/cmdline-user.nix ./home-manager/desktop.nix ./home-manager/desktop-linux.nix ./home-manager/rust-stable.nix];};
           }
         ];
       };
