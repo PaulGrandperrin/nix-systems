c36b5138a2d104c27de0091fdfc23bd240da5187
Paul Grandperrin <paul.grandperrin@gmail.com>
2022-05-03 12:06:08 +0200

    nix-on-droid: fix font check

--- flake.nix
+++ flake.nix
@@ -100,7 +100,7 @@
                     font_src = "${pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; }}/share/fonts/truetype/NerdFonts/Fira Code Regular Nerd Font Complete Mono.ttf";
                     font_dst = "${config.home.homeDirectory}/.termux/font.ttf";
                   in lib.hm.dag.entryAfter ["writeBoundary"] ''
-                    test $(sha1sum ${font_src}|cut -d' ' -f1) != $(sha1sum "${font_dst}" |cut -d' ' -f1) && $DRY_RUN_CMD install $VERBOSE_ARG -D "${font_src}" "${font_dst}"
+                    test $(sha1sum "${font_src}"|cut -d' ' -f1) != $(sha1sum "${font_dst}" |cut -d' ' -f1) && $DRY_RUN_CMD install $VERBOSE_ARG -D "${font_src}" "${font_dst}"
                 '';
               };
               home.packages = [
