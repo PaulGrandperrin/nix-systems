3d0a2b4c3785fd2c7bafd0429fb092427b658ca5
Paul Grandperrin <paul.grandperrin@gmail.com>
2022-05-28 13:57:34 +0200

    HM: fix rust-nightly release

--- home-manager/rust-nightly.nix
+++ home-manager/rust-nightly.nix
@@ -1,12 +1,20 @@
 {pkgs, system, inputs, ...}: let 
 
-  my-rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+  #my-rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+  #  extensions = [
+  #    "rust-src" # needed by rust-analyzer vscode extension when installed through internet
+  #    "rust-analyzer-preview"
+  #  ];
+  #  targets = ["wasm32-unknown-emscripten"];
+  #});
+
+  my-rust = pkgs.rust-bin.nightly."2022-05-28".default.override {
     extensions = [
       "rust-src" # needed by rust-analyzer vscode extension when installed through internet
       "rust-analyzer-preview"
     ];
     targets = ["wasm32-unknown-emscripten"];
-  });
+  };
 
 in {
   home.packages = with pkgs; [
