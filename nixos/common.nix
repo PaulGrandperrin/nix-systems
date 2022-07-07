0589dd8d864a15e36809bdb903034c802c8c6811
Paul Grandperrin <paul.grandperrin@gmail.com>
2022-07-07 14:10:17 +0200

    nixos: add smartd monitoring

--- nixos/common.nix
+++ nixos/common.nix
@@ -128,6 +128,14 @@
   services.smartd = {
     enable = true;
     notifications.mail.enable = true;
+    defaults.monitored = 
+        "-a " # monitor all attributes
+      + "-o on " # enable automatic offline data collection
+      + "-S on " # enable automatic attribute autosave
+      + "-n standby,q " # do not check if disk is in standby, and suppress log message to that effect so as not to cause a write to disk
+      + "-s (S/../.././02|L/../0[1-7]/4/02) " # schedule short self-test every day at 2AM, long self-test every months the first thursday at 2AM
+      + "-W 4,50,55 " # monitor temperature, 4C Diff, 35 Info, 40 Crit
+      ;
   };
 
   ## way too long to build
