b5843c4ea1885039fb991ac8b8f1f2d380b5b063
Paul Grandperrin <paul.grandperrin@gmail.com>
2022-07-08 17:55:13 +0200

    nixos.autoUpgrade: fix

--- nixos/auto-upgrade.nix
+++ nixos/auto-upgrade.nix
@@ -4,7 +4,7 @@
     #flake = "git+ssh://git@github.com/PaulGrandperrin/nixos-conf?ref=main";
     flake = "git+file:///etc/nixos/";
     #flags = [ "--update-input" "nixos" "--commit-lock-file" ];
-    flags = [ "--update-input" "nixos" "--update-input" "flake-utils" "--no-write-lock-file" ];
+    flags = [ "--update-input" "nixos-22-05" "--update-input" "nixos-22-05-small" "--update-input" "nur" "--update-input" "flake-utils" "--no-write-lock-file" ];
     dates = "04:00:00";
     allowReboot = true;
   };
