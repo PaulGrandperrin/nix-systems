4faa48697e1a96b879e55cff94355af88fc1e021
Paul Grandperrin <paul.grandperrin@gmail.com>
2022-08-13 11:49:07 +0200

    nixos: autoUpgrade points to git

--- nixos/auto-upgrade.nix
+++ nixos/auto-upgrade.nix
@@ -1,8 +1,8 @@
 {
   system.autoUpgrade = {
     enable = true;
-    #flake = "git+ssh://git@github.com/PaulGrandperrin/nixos-conf?ref=main";
-    flake = "git+file:///etc/nixos/";
+    flake = "git+ssh://git@github.com/PaulGrandperrin/nix-systems?ref=main";
+    #flake = "git+file:///etc/nixos/";
     #flags = [ "--update-input" "nixos" "--commit-lock-file" ];
     flags = [ "--update-input" "nixos-22-05" "--update-input" "nixos-22-05-small" "--update-input" "nur" "--update-input" "flake-utils" "--no-write-lock-file" ];
     dates = "04:00:00";
