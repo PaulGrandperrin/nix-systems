766401fceee509fdd883ffe2bc5d82a36b8e357c
Paul Grandperrin <paul.grandperrin@gmail.com>
2022-09-02 21:44:58 +0200

    nspawn/ubuntu: open ssh

--- nixos/nspawns/ubuntu.nix
+++ nixos/nspawns/ubuntu.nix
@@ -1,7 +1,13 @@
 { config, pkgs, ... }:
 {
   networking.firewall.trustedInterfaces =  [ "ve-ubuntu" ];
-  systemd.nspawn."ubuntu".enable = true;
+  systemd.nspawn."ubuntu" = {
+    enable = true;
+    # wantedBy = [ "multi-user.target" ]; # doesn't work https://github.com/NixOS/nixpkgs/issues/189499
+    networkConfig = {
+      Port = "tcp:222:22";
+    };
+  };
   systemd.targets.machines.wants = [ "systemd-nspawn@ubuntu.service" ];
 }
 
