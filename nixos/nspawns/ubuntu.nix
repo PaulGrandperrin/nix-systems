20e96373295131074f9407f2427e26aee2fdd4ca
Paul Grandperrin <paul.grandperrin@gmail.com>
2022-09-03 10:31:19 +0200

    nspawn/ubuntu: use nspawn correctly

--- nixos/nspawns/ubuntu.nix
+++ nixos/nspawns/ubuntu.nix
@@ -1,25 +1,24 @@
 { config, pkgs, ... }:
 {
   networking.firewall.trustedInterfaces =  [ "ve-ubuntu" ];
-  #systemd.nspawn."ubuntu" = {
-  #  enable = true;
-  #  # wantedBy = [ "multi-user.target" ]; # doesn't work https://github.com/NixOS/nixpkgs/issues/189499
-  #};
-  environment.etc."systemd/nspawn/ubuntu.nspawn".text= ''
-    [Exec]
-    Hostname=ubuntu
-    #ResolvConf
-
-    [Network]
-    Port=222:22
-    Port=2379
-    Port=20160
-    Port=4000
-    Port=9090
-    Port=3000
-  '';
+  systemd.nspawn."ubuntu" = {
+    enable = true;
+    # wantedBy = [ "multi-user.target" ]; # doesn't work https://github.com/NixOS/nixpkgs/issues/189499
+    execConfig = {
+      Hostname = "ubuntu";
+    };
+    networkConfig = {
+      Port = [
+        "222:22"
+        "2379"  # pd
+        "20160" # tikv
+        "4000"  # tidb
+        "9090"  # prometheus
+        "3000"  # grafana
+      ];
+    };
+  };
   # systemd.services."systemd-nspawn@ubuntu".restartTriggers = [ config.environment.etc."systemd/nspawn/ubuntu.nspawn".source ]; # FIXME breaks the conf because it doesn't understand templates
   systemd.targets.machines.wants = [ "systemd-nspawn@ubuntu.service" ];
-  systemd.targets.multi-user.wants = [ "machines.target" ];
 }
 
