3987fc2a3460a55d3319ca86928c2ffd86f54e03
Paul Grandperrin <paul.grandperrin@gmail.com>
2022-09-05 17:14:23 +0200

    nixos/nspawn: WIP for multiple nspawns

--- nixos/nspawns.nix
+++ nixos/nspawns.nix
@@ -16,7 +16,7 @@ in {
   };
 
   config = mkIf cfg.enable {
-    networking.firewall.trustedInterfaces =  [ "ve-${cfg.name}" ];
+    networking.firewall.trustedInterfaces =  [ "br-my-nspawn" ]; # when using regular veth: "vb-${cfg.name}"
 
     # nix run nixpkgs#debootstrap -- --include=systemd-container --components=main,universe jammy /var/lib/machines/ubuntu http://archive.ubuntu.com/ubuntu/
     # systemctl enable/start systemd-networkd
@@ -25,41 +25,79 @@ in {
     systemd.nspawn."${cfg.name}" = {
       enable = true;
       #wantedBy = [ "multi-user.target" ]; # doesn't work https://github.com/NixOS/nixpkgs/issues/189499
-      #networkConfig = {
-      #  Port = [
-      #    "222:22"
-      #    "2379"  # pd
-      #    "20160" # tikv
-      #    "4000"  # tidb
-      #    "9090"  # prometheus
-      #    "3000"  # grafana
-      #  ];
-      #};
+      networkConfig = {
+        Bridge = "br-my-nspawn";
+        #Port = [
+        #  "222:22"
+        #  "2379"  # pd
+        #  "20160" # tikv
+        #  "4000"  # tidb
+        #  "9090"  # prometheus
+        #  "3000"  # grafana
+        #];
+      };
     };
     # systemd.services."systemd-nspawn@${cfg.name}".restartTriggers = [ config.environment.etc."systemd/nspawn/${cfg.name}.nspawn".source ]; # FIXME breaks the conf because it doesn't understand templates
     systemd.targets.machines.wants = [ "systemd-nspawn@${cfg.name}.service" ];
 
-    systemd.network.networks."05-container-ve-${cfg.name}" = {
-      matchConfig = {
-        "Name" = "ve-${cfg.name}";
-        "Driver" = "veth";
+    services.kea.dhcp4 = {
+      enable = true;
+      settings = {
+        interfaces-config = {
+          interfaces = [
+            "br-my-nspawn"
+          ];
+        };
+        subnet4 = [
+          {
+            pools = [ { pool = "10.0.${toString cfg.net-id}.100 - 10.0.${toString cfg.net-id}.200"; } ];
+            subnet = "10.0.${toString cfg.net-id}.0/24";
+            #interface = "br-my-nspawn";
+            reservations = [
+              {
+                hw-address = "4e:56:49:a0:4f:07";
+                ip-address = "10.0.${toString cfg.net-id}.${toString cfg.id}";
+              }
+            ];
+          }
+        ];
       };
+    };
+
+    systemd.network.netdevs."05-br-my-nspawn".netdevConfig = {
+      Kind = "bridge";
+      Name = "br-my-nspawn";
+    };
+
+    systemd.network.networks."05-br-my-nspawn" = {
+      matchConfig = {
+        Name = "br-my-nspawn";
+        Driver = "bridge";
+      };
+
       networkConfig = {
-        "Address" = "10.0.${toString cfg.net-id}.254/24";
-        "LinkLocalAddressing" = "yes";
-        "DHCPServer" = "yes";
-        "IPMasquerade" = "ipv4";
-        "LLDP" = "yes";
-        "EmitLLDP" = "customer-bridge";
+        Address = "10.0.${toString cfg.net-id}.254/24";
+        LinkLocalAddressing = "yes";
+        #DHCPServer = "yes"; # doesn't work properly. using kea instead. https://github.com/systemd/systemd/issues/21368
+        IPMasquerade = "ipv4";
+        LLDP = "yes";
+        EmitLLDP = "customer-bridge";
       };
       linkConfig = {
         "ActivationPolicy" = "up";
         "RequiredForOnline" = "no";
       };
-      dhcpServerConfig = { # 10.0.${toString cfg.net-id}.${toString cfg.id}
-        PoolOffset = cfg.id;
-        PoolSize = 1;
-      };
+      #dhcpServerConfig = { # 10.0.${toString cfg.net-id}.${toString cfg.id}
+      #  PoolOffset = 100;
+      #  PoolSize = 200;
+      #};
+      #dhcpServerStaticLeases = [{
+      #  dhcpServerStaticLeaseConfig = {
+      #    MACAddress = "4e:56:49:a0:4f:07";
+      #    Address = "10.0.${toString cfg.net-id}.${toString cfg.id}";
+      #  };
+      #}];
+
     };
   };
 }
