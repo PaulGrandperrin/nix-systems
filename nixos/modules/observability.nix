c75859ea39b24aacfa34e888090fa538e447fd4a
Paul Grandperrin <paul.grandperrin@gmail.com>
2022-10-04 22:25:03 +0200

    nixos/observability: add tempo in comments

--- nixos/modules/observability.nix
+++ nixos/modules/observability.nix
@@ -1,12 +1,25 @@
+# We use the Grafana stack, but Uptrace, SigNoz and Jaeger are also interested but not well integrated in NixOS
+
 { config, pkgs, system, inputs, ... }:
 let
   pkgs-unstable = inputs.nixos-unstable.legacyPackages.${system};
 in
 {
 
+  imports = [
+    "${inputs.nixos-unstable.outPath}/nixos/modules/services/tracing/tempo.nix"
+  ];
+
+  nixpkgs.overlays = [(final: prev: { 
+    tempo = pkgs-unstable.tempo;
+    promtail = pkgs-unstable.promtail;
+    loki = pkgs-unstable.loki;
+  })]; 
+
   services = {
     prometheus = {
       enable = true;
+      port = 9090; # default
       package = pkgs-unstable.prometheus;
 
       exporters = {
@@ -106,6 +119,89 @@ in
         };
       };
     };
+
+
+    #tempo = {
+    #  enable = true;
+    #  settings = {
+    #    search_enabled = true;
+    #    
+    #    server = {
+    #      http_listen_port = 3200;
+    #      grpc_listen_port = 9096;
+    #    };
+
+    #    distributor = {
+    #      receivers = {
+    #        otlp = {
+    #          protocols = {
+    #            http.endpoint = "0.0.0.0:4318"; # default
+    #            grpc.endpoint = "0.0.0.0:4317"; # default
+    #          };
+    #        };
+    #      };
+    #    };
+
+    #    #ingester = {
+    #    #  trace_idle_period = "1s";  # the length of time after a trace has not received spans to consider it complete and flush it
+    #    #  max_block_bytes = 1000000; # cut the head block when it hits this size or ...
+    #    #  max_block_duration = "5m"; # this much time passes
+    #    #};
+
+    #    #compactor = {
+    #    #  compaction = {
+    #    #    compaction_window = "1h";              # blocks in this time window will be compacted together
+    #    #    max_block_bytes = 100000000;       # maximum size of compacted blocks
+    #    #    block_retention = "1h";
+    #    #    compacted_block_retention = "10m";
+    #    #  };
+    #    #};
+
+    #    metrics_generator_enabled = true;
+    #    metrics_generator = {
+    #      registry = {
+    #        external_labels = {
+    #          source = "tempo";
+    #        };
+    #      };
+    #      storage = {
+    #        path = "/var/lib/tempo/generator/wal";
+    #        remote_write = [
+    #          {
+    #            url = "http://localhost:${toString config.services.prometheus.port}/api/v1/write";
+    #            #send_exemplars = true;
+    #          }
+    #        ];
+    #      };
+    #    };
+
+    #    storage = {
+    #      trace = {
+    #        backend = "local";                     # backend configuration to use
+    #        #block = {
+    #        #  bloom_filter_false_positive = 0.05; # bloom filter false positive rate.  lower values create larger filters but fewer false positives
+    #        #  index_downsample_bytes = 1000;     # number of bytes per index record
+    #        #  encoding = "zstd";                   # block encoding/compression.  options: none, gzip, lz4-64k, lz4-256k, lz4-1M, lz4, snappy, zstd, s2
+    #        #};
+    #        wal = {
+    #          path = "/var/lib/tempo/wal";             # where to store the the wal locally
+    #          #encoding = "snappy";                 # wal encoding/compression.  options: none, gzip, lz4-64k, lz4-256k, lz4-1M, lz4, snappy, zstd, s2
+    #        };  
+    #        local = {
+    #          path = "/var/lib/tempo/blocks";
+    #        };
+    #        #pool = {
+    #        #  max_workers = 100;                 # worker pool determines the number of parallel requests to the object store backend
+    #        #  queue_depth = 10000;
+    #        #};
+    #      };
+    #    };
+
+    #    overrides = {
+    #      metrics_generator_processors = [ "service-graphs" "span-metrics"];
+    #    };
+    #  };
+    #};
 
     grafana = {
       enable = true;
