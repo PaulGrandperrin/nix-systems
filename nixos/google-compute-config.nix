3487b100e64c35c6853ed169f13856be91d1516f
Paul Grandperrin <paul.grandperrin@gmail.com>
2023-06-05 23:32:49 +0100

    fix ssh warnings

--- nixos/google-compute-config.nix
+++ nixos/google-compute-config.nix
@@ -32,7 +32,7 @@ with lib;
   # and disable password authentication in general
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "prohibit-password";
-  services.openssh.passwordAuthentication = mkDefault false;
+  services.openssh.settings.passwordAuthentication = mkDefault false;
 
   # enable OS Login. This also requires setting enable-oslogin=TRUE metadata on
   # instance or project level
