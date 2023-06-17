7aca40ccb8a8800c50a45e0736189c2d2334ed12
Paul Grandperrin <paul.grandperrin@gmail.com>
2023-06-17 15:42:13 +0200

    nixos-gcp: update hardware-conf

--- nixos/hosts/gcp/hardware-configuration.nix
+++ nixos/hosts/gcp/hardware-configuration.nix
@@ -28,16 +28,25 @@
       fsType = "zfs";
     };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/17E7-9B0C";
-      fsType = "vfat";
-    };
-
   fileSystems."/var/lib/machines/ubuntu" =
     { device = "nixos/ubuntu";
       fsType = "zfs";
     };
 
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/17E7-9B0C";
+      fsType = "vfat";
+    };
+
   swapDevices = [ ];
 
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eth0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wg0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
