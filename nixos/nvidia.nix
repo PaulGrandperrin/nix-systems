e5c049c554e40d3192509004d1ff0abc11e41b0f
Paul Grandperrin <paul.grandperrin@gmail.com>
2023-07-26 14:47:49 +0200

    nvidia: use NixOS' nvidia-offload

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -13,7 +13,10 @@
     modesetting.enable = true; # prime.offload already does it and prime.sync needs it
     nvidiaPersistenced = true; # ensures /sys/class/drm/card0 is nvidia card, and so /dev/nvidia0 is created
     prime = {
-      offload.enable = true; # enables nvidia-drm.modeset just like modesetting.enable
+      offload = {
+        enable = true; # enables nvidia-drm.modeset just like modesetting.enable
+        enableOffloadCmd = true;
+      };
       #sync.enable = true; #  needs modesetting.enable
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
@@ -24,17 +27,6 @@
 
   virtualisation.docker.enableNvidia = true;
 
-  environment.systemPackages = with pkgs; [
-     # nvidia-offload
-     (pkgs.writeShellScriptBin "nvidia-offload" ''
-      export __NV_PRIME_RENDER_OFFLOAD=1
-      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-      export __GLX_VENDOR_LIBRARY_NAME=nvidia
-      export __VK_LAYER_NV_optimus=NVIDIA_only
-      exec -a "$0" "$@"
-    '')
-
-  ];
 }
 
 
