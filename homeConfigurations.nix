152e91f30cc1fad24c354b4301d16b2d5a830289
Paul Grandperrin <paul.grandperrin@gmail.com>
2023-10-18 23:43:42 +0200

    home: generate all combinations

--- homeConfigurations.nix
+++ homeConfigurations.nix
@@ -1,4 +1,5 @@
 inputs: let 
+  lib = inputs.nixos-23-05-lib.lib;
   mkHomeConf = stability: system: username: module: let 
     selectFlake = stable: unstable: { inherit stable unstable; }.${stability};
     nixos-flake = selectFlake inputs.nixos-23-05 inputs.nixos-unstable;
@@ -16,6 +17,23 @@ inputs: let
         ./homeModules/standalone.nix
       ];
     };
-in {
-  unstable-x86_64-linux-paulg = mkHomeConf "unstable" "x86_64-linux" "paulg" ./homeModules/standalone.nix;
-}
+in 
+  # Generate all combinations in the form:
+  # {
+  #   stable-x86_64-linux-root = mkHomeConf "stable" "x86_64-linux" "root" ./homeModules/standalone.nix;
+  #   ...
+  # }
+  builtins.listToAttrs (
+    map 
+      ({stability, system, username}: {
+        name = "${stability}-${system}-${username}";
+        value = true; #mkHomeConf stability system username ./homeModules/standalone.nix;
+      }) 
+      (lib.cartesianProductOfSets {
+        stability = ["stable" "unstable"];
+        system    = lib.systems.flakeExposed;
+        username  = ["root" "paulg"];
+      })
+  )
+  
+
