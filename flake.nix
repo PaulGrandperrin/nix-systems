5a976c42fd2ce5c9114ca45d2ae7da7511f2ad2c
Paul Grandperrin <paul.grandperrin@gmail.com>
2023-10-20 15:16:16 +0200

    flake: cleanup inputs

--- flake.nix
+++ flake.nix
@@ -23,123 +23,137 @@
       id   = "nixpkgs";
     };
 
-    nixos-23-05.url = "github:NixOS/nixpkgs/nixos-23.05";
-    nixos-23-05-lib.url = "github:NixOS/nixpkgs/nixos-23.05?dir=lib"; # "github:nix-community/nixpkgs.lib" doesn't work
-    nixos-23-05-small.url = "github:NixOS/nixpkgs/nixos-23.05-small";
-    darwin-23-05.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
-    darwin-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; # darwin-unstable for now (https://github.com/NixOS/nixpkgs/issues/107466)
-    nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    #nixos-unstable-small.url = "github:NixOS/nixpkgs/nixos-unstable-small";
-    #master.url = "github:NixOS/nixpkgs/master";
+    nixos-23-05.url           = github:NixOS/nixpkgs/nixos-23.05;
+    nixos-23-05-lib.url       = github:NixOS/nixpkgs/nixos-23.05?dir=lib; # "github:nix-community/nixpkgs.lib" doesn't work
+    nixos-23-05-small.url     = github:NixOS/nixpkgs/nixos-23.05-small;
+    darwin-23-05.url          = github:NixOS/nixpkgs/nixpkgs-23.05-darwin;
+    darwin-unstable.url       = github:NixOS/nixpkgs/nixpkgs-unstable; # darwin-unstable for now (https://github.com/NixOS/nixpkgs/issues/107466)
+    nixos-unstable.url        = github:NixOS/nixpkgs/nixos-unstable;
+    #nixos-unstable-small.url = github:NixOS/nixpkgs/nixos-unstable-small;
+    #master.url               = github:NixOS/nixpkgs/master;
 
-    nur.url = "github:nix-community/NUR";
+    nur.url = github:nix-community/NUR;
 
     nix-on-droid = {
-      url = "github:t184256/nix-on-droid/testing";
-      inputs.nixpkgs.follows = "nixos-23-05-lib";
-      inputs.home-manager.follows = "home-manager-23-05"; # TODO try to remove
+      url = github:t184256/nix-on-droid/testing;
+      inputs = {
+        nixpkgs.follows = "nixos-23-05-lib";
+        home-manager.follows = "home-manager-23-05"; # TODO try to remove
+      };
     };
 
     nix-darwin = {
-      url = "github:lnl7/nix-darwin/master";
+      url = github:lnl7/nix-darwin/master;
       inputs.nixpkgs.follows = "nixos-23-05-lib";
     };
 
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
-
     devshell = {
-      url = "github:numtide/devshell";
+      url = github:numtide/devshell;
       inputs.nixpkgs.follows = "nixos-23-05-lib";
     };
 
     rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixos-23-05-lib";
-      inputs.flake-utils.follows = "flake-utils";
+      url = github:oxalica/rust-overlay;
+      inputs = {
+        nixpkgs.follows = "nixos-23-05-lib";
+        flake-utils.follows = "flake-utils";
+      };
     };
 
     home-manager-23-05 = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = github:nix-community/home-manager/release-23.05;
       inputs.nixpkgs.follows = "nixos-23-05-lib"; # not needed by NixOS' module thanks to `home-manager.useGlobalPkgs = true` but needed by the unpriviledged module
     };
     home-manager-master = {
-      url = "github:nix-community/home-manager/master";
+      url = github:nix-community/home-manager/master;
       inputs.nixpkgs.follows = "nixos-23-05-lib"; # not needed by NixOS' module thanks to `home-manager.useGlobalPkgs = true` but needed by the unpriviledged module
     };
 
     nix-alien = {
-      url = "github:thiagokokada/nix-alien";
-      inputs.nixpkgs.follows = "nixos-23-05-lib";
-      inputs.flake-compat.follows = "flake-compat";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.nix-index-database.follows = "nix-index-database";
+      url = github:thiagokokada/nix-alien;
+      inputs = {
+        nixpkgs.follows = "nixos-23-05-lib";
+        flake-compat.follows = "flake-compat";
+        flake-utils.follows = "flake-utils";
+        nix-index-database.follows = "nix-index-database";
+      };
     };
 
     nixgl = {
-      url = "github:guibou/nixGL";
+      url = github:guibou/nixGL;
       flake = false; # TODO it's now a flake!
     };
 
     sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = ""; # optional, not necessary for the module
-      inputs.nixpkgs-stable.follows = ""; # optional, not necessary for the module
+      url = github:Mic92/sops-nix;
+      inputs  = {
+        nixpkgs.follows = ""; # optional, not necessary for the module
+        nixpkgs-stable.follows = ""; # optional, not necessary for the module
+      };
     };
 
     pre-commit-hooks-nix = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      inputs.nixpkgs.follows = "nixos-23-05-lib";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.flake-compat.follows = "flake-compat";
-      inputs.nixpkgs-stable.follows = "nixos-23-05-lib";
+      url = github:cachix/pre-commit-hooks.nix;
+      inputs = {
+        nixpkgs.follows = "nixos-23-05-lib";
+        flake-utils.follows = "flake-utils";
+        flake-compat.follows = "flake-compat";
+        nixpkgs-stable.follows = "nixos-23-05-lib";
+      };  
     };
 
     nix-index-database = {
-      url = "github:Mic92/nix-index-database";
+      url = github:Mic92/nix-index-database;
       inputs.nixpkgs.follows = "nixos-unstable"; # NOTE doesn't only use the lib
     };
 
     nixos-generators = {
-      url = "github:nix-community/nixos-generators";
+      url = github:nix-community/nixos-generators;
       inputs.nixpkgs.follows = "nixos-unstable"; # NOTE doesn't only use the lib
     };
 
     xremap-flake = {
-      url = "github:xremap/nix-flake";
-      inputs.nixpkgs.follows = "nixos-unstable"; # NOTE doesn't only use the lib
-      inputs.home-manager.follows = "home-manager-23-05";
-      inputs.flake-parts.follows = "flake-parts";
-      inputs.devshell.follows = "devshell";
-      inputs.hyprland.follows = ""; # we don't use it
-      inputs.naersk.follows = "naersk";
+      url = github:xremap/nix-flake;
+      inputs = {
+        nixpkgs.follows = "nixos-unstable"; # NOTE doesn't only use the lib
+        home-manager.follows = "home-manager-23-05";
+        flake-parts.follows = "flake-parts";
+        devshell.follows = "devshell";
+        hyprland.follows = ""; # we don't use it
+        naersk.follows = "naersk";
+      };
     };
 
     naersk = {
-      url = "github:nmattia/naersk";
+      url = github:nmattia/naersk;
       inputs.nixpkgs.follows = "nixos-23-05-lib";
     };
 
+    flake-utils = {
+      url = github:numtide/flake-utils;
+    };
+
     flake-compat = {
-      url = "github:edolstra/flake-compat";
+      url = github:edolstra/flake-compat;
       flake = false;
     };
 
     flake-parts = {
-      url = "github:hercules-ci/flake-parts";
+      url = github:hercules-ci/flake-parts;
       inputs.nixpkgs-lib.follows = "nixos-23-05-lib";
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.3.0";
+      url = github:nix-community/lanzaboote/v0.3.0;
 
-      inputs.nixpkgs.follows = "nixos-unstable"; # NOTE doesn't only use the lib
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.rust-overlay.follows = "rust-overlay";
-      inputs.flake-compat.follows = "flake-compat";
-      inputs.flake-parts.follows = "flake-parts";
-      inputs.pre-commit-hooks-nix.follows = "pre-commit-hooks-nix";
+      inputs = {
+        nixpkgs.follows = "nixos-unstable"; # NOTE doesn't only use the lib
+        flake-utils.follows = "flake-utils";
+        rust-overlay.follows = "rust-overlay";
+        flake-compat.follows = "flake-compat";
+        flake-parts.follows = "flake-parts";
+        pre-commit-hooks-nix.follows = "pre-commit-hooks-nix";
+      };
     };
   };
 
