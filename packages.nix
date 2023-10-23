e8e51cdd493f4759a1736d9adf05820c940795ca
Paul Grandperrin <paul.grandperrin@gmail.com>
2023-10-23 19:52:44 +0200

    packages: gen all systems

--- packages.nix
+++ packages.nix
@@ -1,11 +1,13 @@
 inputs: let 
   lib = inputs.nixos-23-05-lib.lib;
-in {
-  x86_64-linux = let 
-    pkgs = import inputs.nixpkgs {
-      system = "x86_64-linux";
-      config = import ./nixpkgs/config.nix;
-    };
-  in
-    (import ./packages pkgs);
-}
+in lib.genAttrs
+      lib.systems.flakeExposed
+      (system:
+        let 
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            config = import ./nixpkgs/config.nix;
+          };
+        in
+          (import ./packages pkgs)
+)
