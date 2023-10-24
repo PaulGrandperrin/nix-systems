76126b05b969e18dd745aaaeb1fba4440c3eb8e4
Paul Grandperrin <paul.grandperrin@gmail.com>
2023-10-24 22:20:31 +0200

    home: add comment

--- flake.nix
+++ flake.nix
@@ -197,8 +197,7 @@
     nixOnDroidConfigurations = import ./nixOnDroidConfigurations.nix inputs; # nix-on-droid switch --flake github:PaulGrandperrin/nix-systems
     darwinConfigurations     = import ./darwinConfigurations.nix     inputs;
     nixosConfigurations      = import ./nixosConfigurations.nix      inputs;
-    homeConfigurations       = import ./homeConfigurations.nix       inputs; # nix --extra-experimental-features "nix-command flakes" run home-manager/master -- --extra-experimental-features "nix-command flakes" switch --flake github:PaulGrandperrin/nix-systems#stable-x86_64-linux-paulg
-    
+    homeConfigurations       = import ./homeConfigurations.nix       inputs;     
   };
 }
 
--- homeModules/standalone.nix
+++ homeModules/standalone.nix
@@ -1,6 +1,15 @@
 # to use on Debian
+
 # sudo apt install nix-bin nix-setup-systemd
-# sudo adduser paulg nix-users
+# sudo adduser paulg nix-users # relog
+# nix --extra-experimental-features "nix-command flakes" run home-manager/release-23.05 -- --extra-experimental-features "nix-command flakes" init --switch --flake github:PaulGrandperrin/nix-systems#stable-x86_64-linux-paulg
+# nix --extra-experimental-features "nix-command flakes" run home-manager/master -- --extra-experimental-features "nix-command flakes" init --switch --flake github:PaulGrandperrin/nix-systems#unstable-x86_64-linux-paulg
+# .nix-profile/bin/fish -l
+
+# . .nix-profile/etc/profile.d/nix.fish
+# . .nix-profile/etc/profile.d/nix.sh
+# . .nix-profile/etc/profile.d/hm-session-vars.sh
+
 
 
 {config, pkgs, inputs, ...}: {
