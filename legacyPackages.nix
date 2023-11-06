a6b5f3822aee74b9bc003789cf49f033d9110d6d
Paul Grandperrin <paul.grandperrin@gmail.com>
2023-11-06 23:27:04 +0100

    legacyPackages: better code

--- legacyPackages.nix
+++ legacyPackages.nix
@@ -1,14 +1,12 @@
 inputs: let
   lib = inputs.nixos-23-05-lib.lib;
-in lib.listToAttrs (
-  map
-    (system: {
-      name = system;
-      value = (import inputs.nixpkgs {
-        inherit system;
-        overlays = [ (import ./overlays.nix inputs).default ];
-        config = import ./nixpkgs/config.nix;
-      });
-    })
-    lib.systems.flakeExposed
-)
+in lib.genAttrs 
+  lib.systems.flakeExposed
+  (system:
+    import inputs.nixpkgs {
+      inherit system;
+      overlays = [ (import ./overlays.nix inputs).default ];
+      config = import ./nixpkgs/config.nix;
+    }
+  )
+
