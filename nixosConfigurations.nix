6213e1cc59d1c318edc7db7e97d2ec92c6963169
Paul Grandperrin <paul.grandperrin@gmail.com>
2023-11-08 21:26:26 +0100

    nixos: leanify

--- nixosConfigurations.nix
+++ nixosConfigurations.nix
@@ -1,24 +1,30 @@
 inputs: let
-  mkNixosConf = stability: module: let
+  mkNixosConf = stability: modules: let
     selectFlake = stable: unstable: { inherit stable unstable; }.${stability}; 
     nixos-flake = selectFlake inputs.nixos-23-05 inputs.nixos-unstable;
     home-manager-flake = selectFlake inputs.home-manager-23-05 inputs.home-manager-master;
   in
     nixos-flake.lib.nixosSystem {
       specialArgs = { inherit inputs nixos-flake home-manager-flake;}; #  passes inputs and main flakes to modules
-      modules = [
-        module
-      ];
+      inherit modules;
     };
 in { 
-  nixos-nas       = mkNixosConf "stable"   ./nixosModules/nixos-nas/configuration.nix;
-  nixos-macmini   = mkNixosConf "stable"   ./nixosModules/nixos-macmini/configuration.nix;
-  nixos-gcp       = mkNixosConf "stable"   ./nixosModules/nixos-gcp/configuration.nix;
-  nixos-oci       = mkNixosConf "stable"   ./nixosModules/nixos-oci/configuration.nix;
-  nixos-xps       = mkNixosConf "unstable" ./nixosModules/nixos-xps/configuration.nix;
-  nixos-macbook   = mkNixosConf "unstable" ./nixosModules/nixos-macbook/configuration.nix;
-  nixos-testvm    = mkNixosConf "unstable" ./nixosModules/nixos-testvm/configuration.nix;
-  nixos-chromebox = mkNixosConf "unstable" ./nixosModules/nixos-chromebox/configuration.nix;
+  nixos-nas            = mkNixosConf "stable"   [ ./nixosModules/nixos-nas/configuration.nix                                         ];
+  nixos-nas-lean       = mkNixosConf "stable"   [ ./nixosModules/nixos-nas/configuration.nix       ./nixosModules/shared/leanify.nix ];
+  nixos-macmini        = mkNixosConf "stable"   [ ./nixosModules/nixos-macmini/configuration.nix                                     ];
+  nixos-macmini-lean   = mkNixosConf "stable"   [ ./nixosModules/nixos-macmini/configuration.nix   ./nixosModules/shared/leanify.nix ];
+  nixos-gcp            = mkNixosConf "stable"   [ ./nixosModules/nixos-gcp/configuration.nix                                         ];
+  nixos-gcp-lean       = mkNixosConf "stable"   [ ./nixosModules/nixos-gcp/configuration.nix       ./nixosModules/shared/leanify.nix ];
+  nixos-oci            = mkNixosConf "stable"   [ ./nixosModules/nixos-oci/configuration.nix                                         ];
+  nixos-oci-lean       = mkNixosConf "stable"   [ ./nixosModules/nixos-oci/configuration.nix       ./nixosModules/shared/leanify.nix ];
+  nixos-xps            = mkNixosConf "unstable" [ ./nixosModules/nixos-xps/configuration.nix                                         ];
+  nixos-xps-lean       = mkNixosConf "unstable" [ ./nixosModules/nixos-xps/configuration.nix       ./nixosModules/shared/leanify.nix ];
+  nixos-macbook        = mkNixosConf "unstable" [ ./nixosModules/nixos-macbook/configuration.nix                                     ];
+  nixos-macbook-lean   = mkNixosConf "unstable" [ ./nixosModules/nixos-macbook/configuration.nix   ./nixosModules/shared/leanify.nix ];
+  nixos-testvm         = mkNixosConf "unstable" [ ./nixosModules/nixos-testvm/configuration.nix                                      ];
+  nixos-testvm-lean    = mkNixosConf "unstable" [ ./nixosModules/nixos-testvm/configuration.nix    ./nixosModules/shared/leanify.nix ];
+  nixos-chromebox      = mkNixosConf "unstable" [ ./nixosModules/nixos-chromebox/configuration.nix                                   ];
+  nixos-chromebox-lean = mkNixosConf "unstable" [ ./nixosModules/nixos-chromebox/configuration.nix ./nixosModules/shared/leanify.nix ];
 }
 
 
---
+++ nixosModules/shared/leanify.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, lib, inputs, ... }: {
+
+  # the specializations multiplies the evaluations
+  specialisation = lib.mkForce {};
+
+  home-manager.users = {
+    # fish wants to regenerate all cache to have autocompletion of manual pages
+    paulg.programs.man.generateCaches = lib.mkForce false;
+    root.programs.man.generateCaches = lib.mkForce false;
+
+    # disable fish generation of completions
+    paulg.xdg.dataFile."fish/home-manager_generated_completions".enable = lib.mkForce false;
+    root.xdg.dataFile."fish/home-manager_generated_completions".enable = lib.mkForce false;
+  };
+}
+
