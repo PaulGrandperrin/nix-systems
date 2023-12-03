6e181286d599408e9c56b0900645381575971213
Paul Grandperrin <paul.grandperrin@gmail.com>
2023-12-03 21:12:42 +0100

    overlays: patched hostapd for intel LAR

--- overlays.nix
+++ overlays.nix
@@ -17,11 +17,27 @@ inputs: rec {
   devenv = (final: prev: {
     devenv = inputs.devenv.packages.${prev.stdenv.hostPlatform.system}.devenv;
   });
+  hostapd = (final: prev: {
+    hostapd = prev.hostapd.overrideAttrs (oldAttrs: {
+      #patches = oldAttrs.patches ++ [
+      patches = [
+        (prev.fetchpatch { # hack to work with intel LAR
+          url = "https://raw.githubusercontent.com/openwrt/openwrt/eefed841b05c3cd4c65a78b50ce0934d879e6acf/package/network/services/hostapd/patches/300-noscan.patch";
+          hash = "sha256-q9yWc5FYhzUFXNzkVIgNe6gxJyE1hQ/iShEluVroiTE=";
+          #url = "https://tildearrow.org/storage/hostapd-2.10-lar.patch";
+          #hash = "sha256-USiHBZH5QcUJfZSxGoFwUefq3ARc4S/KliwUm8SqvoI=";
+          #url = "https://github.com/coolsnowwolf/lede/files/9388276/999-hostapd-2.10-lar.patch.txt";
+          #hash = "sha256-V/dPSjkcCNmj3HK85LD1PFVRgod7U4nh15sKbobfx5A=";
+        })
+      ];
+    });
+  });
   default = inputs.nixos-stable.lib.composeManyExtensions [
     all-channels
     local-packages
     rclonefs
     devenv
+    #hostapd
 
     inputs.nur.overlay
     inputs.rust-overlay.overlays.default
