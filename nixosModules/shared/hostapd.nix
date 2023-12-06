338e077475e4296eb666726269e3e53ab477dcd3
Paul Grandperrin <paul.grandperrin@gmail.com>
2023-12-06 20:42:21 +0100

    nixos/hostapd: wifi 6 with MT7922

--- nixosModules/shared/hostapd.nix
+++ nixosModules/shared/hostapd.nix
@@ -24,43 +24,75 @@
       wifi4 = {
         enable = true;
         capabilities = [
-          # iw phy phy0 info: Capabilities: 0x19ef
           # from https://github.com/openwrt/openwrt/blob/main/package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
+          "HT40-"
           "LDPC"
           "SHORT-GI-20"
           "SHORT-GI-40"
           "TX-STBC"
-          "RX-STBC1"
+          #"RX-STBC123" # not supported by driver
           "MAX-AMSDU-7935"
           "DSSS_CCK-40"
-          "HT40-"
-          #"HT40+"
         ];
       };
       wifi5 = {
         enable = true;
         operatingChannelWidth = "80";
         capabilities = [
-          # iw phy phy0 info: Capabilities: 0x339071b2
           # from https://github.com/openwrt/openwrt/blob/main/package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
           "RXLDPC"
           "SHORT-GI-80"
+          "SHORT-GI-160"
           "TX-STBC-2BY1"
           "SU-BEAMFORMEE"
           "MU-BEAMFORMEE"
           "RX-ANTENNA-PATTERN"
           "TX-ANTENNA-PATTERN"
           "RX-STBC-1"
+          "BF-ANTENNA-4"
+          "VHT160"
           "MAX-MPDU-11454"
           "MAX-A-MPDU-LEN-EXP7"
-          "BF-ANTENNA-4" # really?
         ];
+      };
+      wifi6 = {
+        enable = true;
+        operatingChannelWidth = "80";
       };
       countryCode = "FR";
       channel = 48;
       band = "5g";
       settings = {
-        vht_oper_centr_freq_seg0_idx = 42; # needed by wifi 5 80Mhz
+        vht_oper_centr_freq_seg0_idx=42;
+        he_oper_centr_freq_seg0_idx=42;
+        #he_bss_color=128; # nl80211: kernel reports: integer out of range. Failed to set beacon parameters
+        he_spr_sr_control=3;
+        he_default_pe_duration=4;
+        he_rts_threshold=1023;
+        he_mu_edca_qos_info_param_count=0;
+        he_mu_edca_qos_info_q_ack=0;
+        he_mu_edca_qos_info_queue_request=0;
+        he_mu_edca_qos_info_txop_request=0;
+        he_mu_edca_ac_be_aifsn=8;
+        he_mu_edca_ac_be_aci=0;
+        he_mu_edca_ac_be_ecwmin=9;
+        he_mu_edca_ac_be_ecwmax=10;
+        he_mu_edca_ac_be_timer=255;
+        he_mu_edca_ac_bk_aifsn=15;
+        he_mu_edca_ac_bk_aci=1;
+        he_mu_edca_ac_bk_ecwmin=9;
+        he_mu_edca_ac_bk_ecwmax=10;
+        he_mu_edca_ac_bk_timer=255;
+        he_mu_edca_ac_vi_ecwmin=5;
+        he_mu_edca_ac_vi_ecwmax=7;
+        he_mu_edca_ac_vi_aifsn=5;
+        he_mu_edca_ac_vi_aci=2;
+        he_mu_edca_ac_vi_timer=255;
+        he_mu_edca_ac_vo_aifsn=5;
+        he_mu_edca_ac_vo_aci=3;
+        he_mu_edca_ac_vo_ecwmin=5;
+        he_mu_edca_ac_vo_ecwmax=7;
+        he_mu_edca_ac_vo_timer=255;
       };
       networks.wlp1s0 = {
         ssid = "DelPuppo 5GHz";
@@ -68,7 +100,7 @@
           mode = "wpa3-sae-transition"; # WPA2+3
           wpaPasswordFile = config.sops.secrets."hostapd-password".path; # WPA2 password
           saePasswordsFile = config.sops.secrets."hostapd-password".path; # WPA3 password
-          enableRecommendedPairwiseCiphers = false; # unsupported 
+          enableRecommendedPairwiseCiphers = true;
         };
       };
     };
