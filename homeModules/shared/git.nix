36b884e66a84159ab9743ddfdb4db38a95b7f689
Paul Grandperrin <paul.grandperrin@gmail.com>
2024-02-14 00:11:44 +0100

    home/git: add aliases and conf

--- homeModules/shared/git.nix
+++ homeModules/shared/git.nix
@@ -12,6 +12,12 @@
       enable = true;
       userName = "Paul Grandperrin";
       userEmail = "paul.grandperrin@gmail.com";
+      aliases = {
+        pushf = "push --force-with-lease"; # only force pushes if no new commits have pushed after the last pull
+        blamex = "blame -w -C -C -C"; # ignores whitespaces and code that has just been moved around
+        diffw = "diff --word-diff"; # inline diff
+        stasha = "stash --all"; # stash even untracked and ignored files
+      };
       delta = {
         enable = true;
         options = {
@@ -29,6 +35,13 @@
         init.defaultBranch = "main";
         pull.ff = "only";
         merge.conflictstyle = "diff3";
+        rerere = {
+          enabled = true;
+          autoUpdate = true;
+        };
+        column.ui = "auto";
+        branch.sort = "-committerdate";
+        #core.fsmonitor = true; # watch FS for faster git status
       };
       signing = {
         key = "4AB1353033774DA3";
