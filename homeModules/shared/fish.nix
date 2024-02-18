e91e5c9cbc416059b2736b2369877e88dbb80b8d
Paul Grandperrin <paul.grandperrin@gmail.com>
2024-02-18 13:26:28 +0100

    home/fish: nixos-test abbr

--- homeModules/shared/fish.nix
+++ homeModules/shared/fish.nix
@@ -210,7 +210,8 @@ args @ {pkgs, lib, ...}: {
       ssh-keygen-ed25519 = "ssh-keygen -t ed25519";
       nixos-rebuild-gcp = "nixos-rebuild --flake git+file:///etc/nixos#nixos-gcp --use-substitutes --target-host root@paulg.fr";
       update-hardware-conf = "nixos-generate-config --show-hardware-config --no-filesystems > /etc/nixos/nixosModules/$(hostname)/hardware-configuration.nix && git -C /etc/nixos/ commit /etc/nixos/nixosModules/$(hostname)/hardware-configuration.nix -m \"$(hostname): update hardware-configuration.nix\"";
-      update-nixos-flake = "pushd /etc/nixos && nix flake update && git commit -m \"nix flake update\" flake.lock && git push && popd";
+      nixos-update-flake = "pushd /etc/nixos && nix flake update && git commit -m \"nix flake update\" flake.lock && git push && popd";
+      nixos-test = "nixos-rebuild test --flake /etc/nixos/#$(hostname)-lean -L";
       clean-gcroots = "find -L /nix/var/nix/gcroots/per-user/$USER -maxdepth 1 -type l -delete";
     };
     plugins = with pkgs.fishPlugins; [
