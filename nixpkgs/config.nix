d538f054d0c01e8b3b402cac5f31e7e5844606d8
Paul Grandperrin <paul.grandperrin@gmail.com>
2024-03-21 19:23:47 +0100

    fix nixd insecure dep

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,4 +1,7 @@
 {
   allowUnfree = true;
   android_sdk.accept_license = true;
+  permittedInsecurePackages = [
+    "nix-2.15.3" "nix-2.16.2" # https://github.com/nix-community/nixd/issues/357
+  ];
 }
