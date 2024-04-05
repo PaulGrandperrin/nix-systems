429cd1c73ede64c0633aaffdadff9937d8fa7faa
Paul Grandperrin <paul.grandperrin@gmail.com>
2024-04-05 08:29:40 +0200

    nixos-oci: add postgres

--- nixosModules/nixos-oci/configuration.nix
+++ nixosModules/nixos-oci/configuration.nix
@@ -74,6 +74,20 @@
     restartUnits = [ "yuzu-multiplayer.service" ];
   };
 
+  services.postgresql = {
+    enable = true;
+    enableTCPIP = true;
+    authentication = ''
+      host all all 10.42.0.0/16  trust # trust VPN
+      local all all              trust
+      host  all all 127.0.0.1/32 trust
+      host  all all ::1/128      trust
+    '';
+    ensureDatabases = [
+      "nar-dedup"
+    ];
+  };
+
   services.yuzu = {
     #enable = true; # removed from nixpkgs
     package = pkgs.my-yuzu;
