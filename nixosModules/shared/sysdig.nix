d2239dc4666b2e037846024b007c32374be43988
Paul Grandperrin <paul.grandperrin@gmail.com>
2024-04-08 21:10:50 +0200

    nixos/sysdig: simplify

--- nixosModules/shared/sysdig.nix
+++ nixosModules/shared/sysdig.nix
@@ -1,6 +1,7 @@
 {pkgs, config, lib, ...}: {
-  config = lib.mkIf (pkgs.stdenv.hostPlatform.system == "x86_64-linux") (
-    let sysdig = pkgs.unstable.callPackage (import (pkgs.unstable.path + "/pkgs/os-specific/linux/sysdig")) {kernel = config.boot.kernelPackages.kernel;};
+  config = lib.mkIf (pkgs.stdenv.hostPlatform.system == "x86_64-linux") ( let
+    #sysdig = pkgs.unstable.callPackage (import (pkgs.unstable.path + "/pkgs/os-specific/linux/sysdig")) {kernel = config.boot.kernelPackages.kernel;};
+    sysdig = pkgs.unstable.sysdig.override {kernel = config.boot.kernelPackages.kernel;};
   in {
     #programs.sysdig.enable = true;
     environment.systemPackages = [ sysdig ];
