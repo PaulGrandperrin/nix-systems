f55d440e2d07d6d0cbf4a55c923b10c558f47461
Paul Grandperrin <paul.grandperrin@gmail.com>
2024-09-24 13:09:38 +0200

    nixpkgs: remove outdated conf

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,7 +1,4 @@
 {
   allowUnfree = true;
   android_sdk.accept_license = true;
-  permittedInsecurePackages = [
-    "nix-2.15.3" "nix-2.16.2" # https://github.com/nix-community/nixd/issues/357
-  ];
 }
