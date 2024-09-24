865046681a9e63e12b95ad062e779dbcad2c47b3
Paul Grandperrin <paul.grandperrin@gmail.com>
2024-09-24 23:45:18 +0200

    system-manager: install itself in default module

--- systemConfigs.nix
+++ systemConfigs.nix
@@ -1,5 +1,8 @@
 inputs: {
   default = inputs.system-manager.lib.makeSystemConfig {
+    extraSpecialArgs = {
+      inherit inputs;
+    }; 
     modules = [
       ./systemModules
     ];
--- systemModules/default.nix
+++ systemModules/default.nix
@@ -1,3 +1,15 @@
-{config, lib, pkgs, ...}: {
+{config, lib, pkgs, inputs, ...}: {
+  nixpkgs.hostPlatform = "x86_64-linux";
+  system-manager.allowAnyDistro = true;
 
+  environment = {
+    etc = {
+      "foo.conf".text = ''
+        launch_the_rockets = true
+      '';
+    };
+    systemPackages = [
+      inputs.system-manager.packages.${pkgs.stdenv.hostPlatform.system}.system-manager
+    ];
+  };
 }
