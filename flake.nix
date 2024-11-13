c15f0d5f46b7d7dc4b234e9b985ab1e2dc60bf50
Paul Grandperrin <paul.grandperrin@gmail.com>
2024-11-13 01:47:04 +0100

    add sops-nix cache

--- flake.nix
+++ flake.nix
@@ -8,11 +8,15 @@
       "https://nix-community.cachix.org"
       "https://cache.nixos.org"
       "https://devenv.cachix.org"
+      "https://cosmic.cachix.org/"
+      "https://cache.thalheim.io" # sops-nix
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nas.grandperrin.fr:QwhwNrClkzxCvdA0z3idUyl76Lmho6JTJLWplKtC2ig="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+      "cache.thalheim.io-1:R7msbosLEZKrxk/lKxf9BTjOOH7Ax3H0Qj0/6wiHOgc=" # sops-nix
     ];
   };
 
