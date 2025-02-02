1dafc405f6ab6e494f0d30a938e4e089fee8d237
Paul Grandperrin <paul.grandperrin@gmail.com>
2025-02-02 16:12:57 +0100

    packages/foundationdb-bin: make multiarch

--- packages/foundationdb-bin/package.nix
+++ packages/foundationdb-bin/package.nix
@@ -9,20 +9,47 @@
 
   xz,
   zlib,
+
+  version ? "7.3.57",
 }: let
   meta = with lib; {
     description = "Open source, distributed, transactional key-value store";
     homepage    = "https://www.foundationdb.org";
     license     = licenses.asl20;
-    platforms   = [ "x86_64-linux" ];
+    platforms   = [ "x86_64-linux" "aarch64-linux" ];
   };
+
+  versions = {
+    "7.3.59" = { # pre-release
+      x86_64 = {
+        clients = "sha256-qGRMZrGK9z+ATpuHF64+hfbUCke+0130lIhxUpwOvtY=";
+        server = "sha256-HKD7oJLb1bZy4KJPprWxItcifdfqPdvhnYn50LNMZuU=";
+      };
+      aarch64 = {
+        clients = "";
+        server = "";
+      };
+    };
+    "7.3.57" = { # latest
+      x86_64 = {
+        clients = "sha256-v1ssLbMb/0J2gl3zfSka7YBDF3a6Mz+FMKD3xa7sAR0=";
+        server = "sha256-ys7WnJOdqwjJtNx2N/NrplIvPfpTUHFlW7uFkCz5Ra0=";
+      };
+    };
+  };
+
+  arch = stdenvNoCC.hostPlatform.linuxArch;
+  arch_deb = {
+    "x86_64" = "amd64";
+    "aarch64" = "aarch64";
+  }.${arch};
   
-  foundationdb-clients-bin = stdenvNoCC.mkDerivation rec {
-    pname = "foundationdb-clients-bin";
-    version = "7.3.57";
+  mkFoundationdbBin = artifactName: version: stdenvNoCC.mkDerivation {
+    pname = "foundationdb-bin-${artifactName}";
+    inherit version;
     src = fetchurl {
-      url = "https://github.com/apple/foundationdb/releases/download/${version}/foundationdb-clients_${version}-1_amd64.deb";
-      hash = "sha256-v1ssLbMb/0J2gl3zfSka7YBDF3a6Mz+FMKD3xa7sAR0=";
+      url = "https://github.com/apple/foundationdb/releases/download/${version}/foundationdb-${artifactName}_${version}-1_${arch_deb}.deb";
+      hash = versions.${version}.${arch}.${artifactName};
     };
 
     dontBuild = true;
@@ -35,18 +62,16 @@
     installPhase = ''
       runHook preInstall
 
-      ls -R
       mkdir -p $out/{bin,libexec}
-      mv usr/{bin,sbin}/* $out/bin/
-      mv usr/lib/foundationdb/backup_agent/* $out/libexec/
+      find usr/{bin,sbin,lib/foundationdb}/ -maxdepth 1 -type f -executable -print -exec mv '{}' $out/bin/ \; || true
+      find usr/lib/foundationdb/backup_agent/ -maxdepth 1 -type f -executable -print -exec mv '{}' $out/libexec/ \; || true
 
       mkdir -p $dev/lib
-      mv usr/include $dev/
-      mv usr/lib/cmake $dev/lib/
-      mv usr/lib/pkgconfig $dev/lib/
+      find usr/include -maxdepth 0 -type d -print -exec mv '{}' $dev/ \; || true
+      find usr/lib/{cmake,pkgconfig} -maxdepth 0 -type d -print -exec mv '{}' $dev/lib/ \; || true
 
       mkdir -p $lib/lib
-      mv usr/lib/*.so $lib/lib/
+      find usr/lib/ -maxdepth 1 -type f -name "*.so" -print -exec mv '{}' $lib/lib/ \; || true
 
       runHook postInstall
     '';
@@ -56,53 +81,24 @@
     inherit meta;
   };
 
-  foundationdb-server-bin = stdenvNoCC.mkDerivation rec {
-    pname = "foundationdb-server-bin";
-    version = "7.3.57";
-    src = fetchurl {
-      url = "https://github.com/apple/foundationdb/releases/download/${version}/foundationdb-server_${version}-1_amd64.deb";
-      hash = "sha256-ys7WnJOdqwjJtNx2N/NrplIvPfpTUHFlW7uFkCz5Ra0=";
-    };
-
-    dontBuild = true;
-    nativeBuildInputs = [ dpkg autoPatchelfHook ];
-    buildInputs = [
-      xz
-      zlib
-    ];
-
-    installPhase = ''
-      runHook preInstall
-
-      ls -R
-      mkdir -p $out/bin
-      mv usr/{bin,sbin}/* $out/bin/
-      mv usr/lib/foundationdb/fdbmonitor $out/bin/
-
-      runHook postInstall
-    '';
-
-    inherit meta;
-  };
-
-  foundationdb-bin = stdenvNoCC.mkDerivation {
+  foundationdb-bin = stdenvNoCC.mkDerivation rec {
     pname = "foundationdb-bin";
-    version = "7.3.57";
+    inherit version;
     outputs = [ "out" "dev" "lib" ];
 
     buildCommand = ''
       mkdir -p $out
-      for i in ${foundationdb-clients-bin} ${foundationdb-server-bin}; do
+      for i in ${(mkFoundationdbBin "clients" version).out} ${(mkFoundationdbBin "server" version).out}; do
         ${xorg.lndir}/bin/lndir -silent $i $out
       done
 
       mkdir -p $dev
-      for i in ${foundationdb-clients-bin.dev}; do
+      for i in ${(mkFoundationdbBin "clients" version).dev} ${(mkFoundationdbBin "server" version).dev}; do
         ${xorg.lndir}/bin/lndir -silent $i $dev
       done
 
       mkdir -p $lib
-      for i in ${foundationdb-clients-bin.lib}; do
+      for i in ${(mkFoundationdbBin "clients" version).lib} ${(mkFoundationdbBin "server" version).lib}; do
         ${xorg.lndir}/bin/lndir -silent $i $lib
       done
     '';
