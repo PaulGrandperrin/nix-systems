0776db7d5af3ad1e9f0b05a0d0ad6be67e7d9380
Paul Grandperrin <paul.grandperrin@gmail.com>
2025-08-10 10:10:34 +0200

    nixos-macmini: remove unmaintained and insecure broadcom wifi driver

--- nixosModules/nixos-macmini/hardware-configuration.nix
+++ nixosModules/nixos-macmini/hardware-configuration.nix
@@ -11,7 +11,9 @@
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "sd_mod" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" "wl" ];
-  boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+  boot.extraModulePackages = [ 
+    #config.boot.kernelPackages.broadcom_sta # unmaintained and insecure
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
