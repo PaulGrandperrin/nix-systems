ef53c0a3991989d809dbae324aee59c03ac0f0ff
Paul Grandperrin <paul.grandperrin@gmail.com>
2025-09-11 13:39:35 +0200

    nixos-xps: dont use latest kernel which fails to build

--- nixosModules/nixos-xps/configuration.nix
+++ nixosModules/nixos-xps/configuration.nix
@@ -8,7 +8,7 @@
     ../shared/wg-mounts.nix
     ../shared/desktop.nix
     ../shared/gnome.nix
-    ../shared/cosmic.nix
+    #../shared/cosmic.nix
     ../shared/desktop-i915.nix
     ../shared/nvidia.nix
     ../shared/gaming.nix
@@ -23,20 +23,20 @@
   # manually evaluate `latest-zfs-kernel` to set its `pkgs` to `pkgs.unstable`
   #boot.kernelPackages = ((import "${inputs.srvos}/nixos/mixins/latest-zfs-kernel.nix") {inherit lib config; pkgs= pkgs.unstable;}).boot.kernelPackages;
 
-  boot.kernelPackages = let # from https://wiki.nixos.org/wiki/ZFS#Selecting_the_latest_ZFS-compatible_Kernel
-    zfsCompatibleKernelPackages = lib.filterAttrs (
-      name: kernelPackages:
-      (builtins.match "linux_[0-9]+_[0-9]+" name) != null
-      && (builtins.tryEval kernelPackages).success
-      && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
-    ) pkgs.unstable.linuxKernel.packages; # take kernel from unstable
-  in lib.last (
-    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
-      builtins.attrValues zfsCompatibleKernelPackages
-    )
-  );
+  #boot.kernelPackages = let # from https://wiki.nixos.org/wiki/ZFS#Selecting_the_latest_ZFS-compatible_Kernel
+  #  zfsCompatibleKernelPackages = lib.filterAttrs (
+  #    name: kernelPackages:
+  #    (builtins.match "linux_[0-9]+_[0-9]+" name) != null
+  #    && (builtins.tryEval kernelPackages).success
+  #    && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
+  #  ) pkgs.unstable.linuxKernel.packages; # take kernel from unstable
+  #in lib.last (
+  #  lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+  #    builtins.attrValues zfsCompatibleKernelPackages
+  #  )
+  #);
 
-  boot.zfs.package = lib.mkForce pkgs.unstable.zfs; # also take zfs userspace from unstable for versions to be in sync
+  #boot.zfs.package = lib.mkForce pkgs.unstable.zfs; # also take zfs userspace from unstable for versions to be in sync
 
   home-manager.users = let 
     homeModule = {
