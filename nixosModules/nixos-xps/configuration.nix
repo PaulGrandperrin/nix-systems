b57be523a783fcbc538d25261985b2cd488e7e41
Paul Grandperrin <paul.grandperrin@gmail.com>
2025-09-13 20:35:07 +0200

    nixos-xps: go back to COSMIC

--- nixosModules/nixos-xps/configuration.nix
+++ nixosModules/nixos-xps/configuration.nix
@@ -7,8 +7,8 @@
     ../shared/wireguard.nix
     ../shared/wg-mounts.nix
     ../shared/desktop.nix
-    ../shared/gnome.nix
-    #../shared/cosmic.nix
+    #../shared/gnome.nix
+    ../shared/cosmic.nix
     ../shared/desktop-i915.nix
     ../shared/nvidia.nix
     ../shared/gaming.nix
