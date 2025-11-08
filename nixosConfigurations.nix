565e63d4a3a52fdb84e8fc60258024dd45fbcf10
Paul Grandperrin <paul.grandperrin@gmail.com>
2025-11-08 18:06:16 +0100

    nixos: move all machines to unstable

--- nixosConfigurations.nix
+++ nixosConfigurations.nix
@@ -9,24 +9,24 @@ inputs: let
       inherit modules;
     };
 in { 
-  nixos-nas            = mkNixosConf "stable"   [ ./nixosModules/nixos-nas/configuration.nix                                         ];
-  nixos-nas-lean       = mkNixosConf "stable"   [ ./nixosModules/nixos-nas/configuration.nix       ./nixosModules/shared/leanify.nix ];
-  nixos-macmini        = mkNixosConf "stable"   [ ./nixosModules/nixos-macmini/configuration.nix                                     ];
-  nixos-macmini-lean   = mkNixosConf "stable"   [ ./nixosModules/nixos-macmini/configuration.nix   ./nixosModules/shared/leanify.nix ];
-  nixos-gcp            = mkNixosConf "stable"   [ ./nixosModules/nixos-gcp/configuration.nix                                         ];
-  nixos-gcp-lean       = mkNixosConf "stable"   [ ./nixosModules/nixos-gcp/configuration.nix       ./nixosModules/shared/leanify.nix ];
-  nixos-oci            = mkNixosConf "stable"   [ ./nixosModules/nixos-oci/configuration.nix                                         ];
-  nixos-oci-lean       = mkNixosConf "stable"   [ ./nixosModules/nixos-oci/configuration.nix       ./nixosModules/shared/leanify.nix ];
+  nixos-nas            = mkNixosConf "unstable"   [ ./nixosModules/nixos-nas/configuration.nix                                         ];
+  nixos-nas-lean       = mkNixosConf "unstable"   [ ./nixosModules/nixos-nas/configuration.nix       ./nixosModules/shared/leanify.nix ];
+  nixos-macmini        = mkNixosConf "unstable"   [ ./nixosModules/nixos-macmini/configuration.nix                                     ];
+  nixos-macmini-lean   = mkNixosConf "unstable"   [ ./nixosModules/nixos-macmini/configuration.nix   ./nixosModules/shared/leanify.nix ];
+  nixos-gcp            = mkNixosConf "unstable"   [ ./nixosModules/nixos-gcp/configuration.nix                                         ];
+  nixos-gcp-lean       = mkNixosConf "unstable"   [ ./nixosModules/nixos-gcp/configuration.nix       ./nixosModules/shared/leanify.nix ];
+  nixos-oci            = mkNixosConf "unstable"   [ ./nixosModules/nixos-oci/configuration.nix                                         ];
+  nixos-oci-lean       = mkNixosConf "unstable"   [ ./nixosModules/nixos-oci/configuration.nix       ./nixosModules/shared/leanify.nix ];
   nixos-xps            = mkNixosConf "unstable"   [ ./nixosModules/nixos-xps/configuration.nix                                         ];
   nixos-xps-lean       = mkNixosConf "unstable"   [ ./nixosModules/nixos-xps/configuration.nix       ./nixosModules/shared/leanify.nix ];
   nixos-xps2           = mkNixosConf "unstable" [ ./nixosModules/nixos-xps2/configuration.nix                                        ];
   nixos-xps2-lean      = mkNixosConf "unstable" [ ./nixosModules/nixos-xps2/configuration.nix      ./nixosModules/shared/leanify.nix ];
-  nixos-macbook        = mkNixosConf "stable"   [ ./nixosModules/nixos-macbook/configuration.nix                                     ];
-  nixos-macbook-lean   = mkNixosConf "stable"   [ ./nixosModules/nixos-macbook/configuration.nix   ./nixosModules/shared/leanify.nix ];
-  nixos-testvm         = mkNixosConf "stable"   [ ./nixosModules/nixos-testvm/configuration.nix                                      ];
-  nixos-testvm-lean    = mkNixosConf "stable"   [ ./nixosModules/nixos-testvm/configuration.nix    ./nixosModules/shared/leanify.nix ];
-  nixos-chromebox      = mkNixosConf "stable"   [ ./nixosModules/nixos-chromebox/configuration.nix                                   ];
-  nixos-chromebox-lean = mkNixosConf "stable"   [ ./nixosModules/nixos-chromebox/configuration.nix ./nixosModules/shared/leanify.nix ];
+  nixos-macbook        = mkNixosConf "unstable"   [ ./nixosModules/nixos-macbook/configuration.nix                                     ];
+  nixos-macbook-lean   = mkNixosConf "unstable"   [ ./nixosModules/nixos-macbook/configuration.nix   ./nixosModules/shared/leanify.nix ];
+  nixos-testvm         = mkNixosConf "unstable"   [ ./nixosModules/nixos-testvm/configuration.nix                                      ];
+  nixos-testvm-lean    = mkNixosConf "unstable"   [ ./nixosModules/nixos-testvm/configuration.nix    ./nixosModules/shared/leanify.nix ];
+  nixos-chromebox      = mkNixosConf "unstable"   [ ./nixosModules/nixos-chromebox/configuration.nix                                   ];
+  nixos-chromebox-lean = mkNixosConf "unstable"   [ ./nixosModules/nixos-chromebox/configuration.nix ./nixosModules/shared/leanify.nix ];
   nixos-asus           = mkNixosConf "unstable"   [ ./nixosModules/nixos-asus/configuration.nix                                        ];
   nixos-asus-lean      = mkNixosConf "unstable"   [ ./nixosModules/nixos-asus/configuration.nix      ./nixosModules/shared/leanify.nix ];
 }
