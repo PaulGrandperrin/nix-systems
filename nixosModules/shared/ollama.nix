7336ae08d5e1d8a5946ad9f5fdba15458fbcd9ec
Paul Grandperrin <paul.grandperrin@gmail.com>
2025-11-24 10:34:49 +0100

    nixos/ollama: remove vulkan workaround

--- nixosModules/shared/ollama.nix
+++ nixosModules/shared/ollama.nix
@@ -10,19 +10,13 @@ in {
   config = mkIf cfg.enable {
     services.ollama = {
       enable = true;
-      #acceleration = "vulkan"; # waiting for https://github.com/NixOS/nixpkgs/pull/463430
+      acceleration = "vulkan";
     };
 
     services.nextjs-ollama-llm-ui = {
       enable = true;
       hostname = "${config.networking.hostName}.wg";
     };
-
-    # force vulkan until PR is merged
-    services.ollama.package = pkgs.unstable.ollama-vulkan;
-    systemd.services.ollama.environment.OLLAMA_VULKAN = "1";
-    systemd.services.ollama.serviceConfig.ExecStart = lib.mkForce "${lib.getExe pkgs.unstable.ollama-vulkan} serve";
-    environment.systemPackages = [ (lib.hiPrio pkgs.unstable.ollama-vulkan) ];
   };
 }
 
