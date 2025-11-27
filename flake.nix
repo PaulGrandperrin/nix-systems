914af2c207ced8203b70c8c0fd9b94b9b9e1336b
Paul Grandperrin <paul.grandperrin@gmail.com>
2025-11-27 10:57:13 +0100

    flake/HM: use 25.11 stable branch

--- flake.nix
+++ flake.nix
@@ -64,8 +64,7 @@
     };
 
     home-manager-stable = {
-      url = "github:nix-community/home-manager/master";
-      #url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixos-stable-lib"; # not needed by NixOS' module thanks to `home-manager.useGlobalPkgs = true` but needed by the unpriviledged module
     };
     home-manager-unstable = {
