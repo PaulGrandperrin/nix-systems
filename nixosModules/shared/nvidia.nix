014f0b5ab66337bd5343da0790081dca90b3e9dc
Paul Grandperrin <paul.grandperrin@gmail.com>
2025-12-06 09:32:22 +0100

    nixos/nvidia: enable coolbits

--- nixosModules/shared/nvidia.nix
+++ nixosModules/shared/nvidia.nix
@@ -6,6 +6,22 @@
     videoDrivers = ["nvidia" ];
   };
 
+      # taken from https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/misc/tuxclocker.nix
+      # https://download.nvidia.com/XFree86/Linux-x86_64/430.14/README/xconfigoptions.html#Coolbits
+      services.xserver.config =
+        let
+          configSection = (
+            i: ''
+              Section "Device"
+                Driver "nvidia"
+                Option "Coolbits" "31"
+                Identifier "Device-nvidia[${toString i}]"
+              EndSection
+            ''
+          );
+        in
+        lib.concatStrings (map configSection [ 0 ]);
+
   environment.systemPackages = with pkgs; [
     #nvtopPackages.nvidia # depends on CUDA which is broken
   ];
