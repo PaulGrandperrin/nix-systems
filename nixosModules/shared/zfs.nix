217bc11d8668dc16b699d632006eca4f06559e8e
Paul Grandperrin <paul.grandperrin@gmail.com>
2025-12-18 14:05:10 +0100

    nixos/zfs: add zfs creation comments

--- nixosModules/shared/zfs.nix
+++ nixosModules/shared/zfs.nix
@@ -1,5 +1,32 @@
-{ config, pkgs, lib, ... }:
-{
+{ config, pkgs, lib, ... }: {
+
+  # zfs create
+  # -o cachefile=none # stores which zpool should be imported, not needed  when the zpool hosts the OS itself
+  # # -o ashift=12 # 4k sector size. if nvme lbasize is correctly configured, shouldn't be needed. check with zdb -C zpool | grep ashift
+  # -o failmode=continue # allow reads even in case of catastrophic pool failure
+  # -o autotrim=on # fine on modern SSDs
+  # -O mountpoint=none # don't use ZFS auto mount
+  # -O atime=off # avoids writting access time
+  # -O compression=zstd-fast # outperforms lz4 in compression and decompression speed and compression ratio
+  # -O recordsize=1M # precompression maximum size of a record. default is 128k. 1M: better compression, less fragmentation. For bittorrents, use 16k.
+  # -O acltype=posixacl
+  # -O xattr=sa # good perf when using posixacl
+  # -O dnodesize=auto # good perf when using xattr=sa
+  # -O redundant_metadata=most # allows losing more than one record at once in case of corruption, but won't affect multiple files
+  # -O sync=disabled # don't respect synchronous writes. allows losing up to 5 sec of data. renders logbios prop meaningless. helps with perf, write amplification and fragmentation
+  # -O direct=standard # respect direct IO semantics
+
+  # no utf8only or normalization: might break proton/wine
+  # casesensitivity=insensitive: perf boost for proton but breaks unix
+
+  # zfs create zpool/encrypted
+  # -o encryption=on # good default: aes256-gcm
+  # -o keyformat=passphrase
+  # -o pbkdf2iters=1000000 # 3 times the 2026 default
+
+  # zfs create zpool/encrypted/nixos
+  # zfs create zpool/encrypted/nixos/home
+
   boot.zfs.devNodes = "/dev/disk/by-path"; # /dev/disk/by-id doesn't get populated with virtio disks. see https://github.com/NixOS/nixpkgs/pull/263662
 
   # boot.forceImportRoot = false; 
