14b01525251133da9966b1bb684168e1b40bcfdf
Paul Grandperrin <paul.grandperrin@gmail.com>
2026-05-14 16:21:49 +0200

    nixos/gnome: remove nvidia suspend workaround

--- nixosModules/shared/gnome.nix
+++ nixosModules/shared/gnome.nix
@@ -18,43 +18,6 @@
 
   # Gnome shell extensions:
   services.desktopManager.gnome.sessionPath = [];
-
-  # Workaround https://github.com/NixOS/nixpkgs/issues/336723
-  systemd = {
-     services."gnome-suspend" = {
-      description = "suspend gnome shell";
-      before = [
-        "systemd-suspend.service" 
-        "systemd-hibernate.service"
-        "nvidia-suspend.service"
-        "nvidia-hibernate.service"
-      ];
-      wantedBy = [
-        "systemd-suspend.service"
-        "systemd-hibernate.service"
-      ];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = ''${pkgs.procps}/bin/pkill -f -STOP ${pkgs.gnome-shell}/bin/gnome-shell'';
-      };
-    };
-    services."gnome-resume" = {
-      description = "resume gnome shell";
-      after = [
-        "systemd-suspend.service" 
-        "systemd-hibernate.service"
-        "nvidia-resume.service"
-      ];
-      wantedBy = [
-        "systemd-suspend.service"
-        "systemd-hibernate.service"
-      ];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = ''${pkgs.procps}/bin/pkill -f -CONT ${pkgs.gnome-shell}/bin/gnome-shell'';
-      };
-    };
-  };
 }
 
 
