a476a3301f461c1ad990cbc004b78dedfb8fa2de
Paul Grandperrin <paul.grandperrin@gmail.com>
2026-07-31 17:49:20 +0200

    nix/nix: fix cache config

--- nix/nix.nix
+++ nix/nix.nix
@@ -3,18 +3,18 @@
 # can be extended in plain text with nix.extraOptions
 rec {
   experimental-features = "nix-command flakes";
-  auto-optimise-store = true; # maybe causes build failures
+  auto-optimise-store = true;
   allowed-users = [ "@wheel" "nix-serve" ];
   #trusted-users = ["@wheel"]; # only do it on desktop machines
 
-  substituters = [
+  extra-substituters = [
     #"http://nixos-nas.wg:5000" # breaks everything when host is down: https://github.com/NixOS/nix/issues/6901
     "https://nix-community.cachix.org"
     "https://devenv.cachix.org"
     "https://nix-amd-ai.cachix.org"
   ];
-  trusted-substituters = substituters; # my system subtituters can be used by untrusted users
-  trusted-public-keys = [
+  extra-trusted-substituters = extra-substituters; # my system subtituters can be used by untrusted users
+  extra-trusted-public-keys = [
     #"nas.grandperrin.fr:QwhwNrClkzxCvdA0z3idUyl76Lmho6JTJLWplKtC2ig="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
